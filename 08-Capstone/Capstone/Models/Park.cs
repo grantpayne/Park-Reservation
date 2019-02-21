fa4a15523f15018295e0791c39ab65b9a29428f5
Patrick Williams <pwilliams0802@gmail.com>
2019-02-21 14:31:36 -0500

    Created 2 model classes, going to push test merge

---
+++ 08-Capstone/Capstone/Models/Park.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Capstone.Models
+{
+    public class Park
+    {
+        public int ParkID { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public DateTime DateEstablished { get; set; } //TODO double check this data type
+        public int Area { get; set; }
+        public int Visitors { get; set; }
+        public string Description { get; set; }
+
+        public override string ToString() //TODO fill out the override string
+        {
+            return base.ToString();
+        }
+    }
+}
