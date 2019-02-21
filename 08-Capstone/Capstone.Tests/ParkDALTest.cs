2e2a452c4126f3b551bb24469cf31dc121fb2fbd
Patrick Williams <pwilliams0802@gmail.com>
2019-02-21 15:02:58 -0500

    Added unit test classes

---
+++ 08-Capstone/Capstone.Tests/ParkDALTest.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Capstone.Tests
+{
+    [TestClass]
+    public class ParkDALTest
+    {
+        [TestInitialize]
+        public void Initialize()
+        {
+
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
