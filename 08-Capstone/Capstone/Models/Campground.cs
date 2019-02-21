9041db5133b2d88f6d8f1bba5cd484e15e494328
Grant Payne <afroman7600@yahoo.com>
2019-02-21 14:33:25 -0500

    Created Campground and Reservation models

---
+++ 08-Capstone/Capstone/Models/Campground.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Capstone.Models
+{
+    class Campground
+    {
+        public int Campground_id { get; set; }
+        public int Park_id { get; set; }
+        public string Name { get; set; }
+        public DateTime Open_from_mm { get; set; }
+        public DateTime Open_to_mm { get; set; }
+        public decimal Daily_fee { get; set; }
+
+        public override string ToString() //TODO: Campground ToString Fotmatting
+        {
+            return "";
+        }
+    }
+}
---
+++ 08-Capstone/Capstone/Models/Reservation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Capstone.Models
+{
+    class Reservation
+    {
+        public int Reservation_id { get; set; }
+        public int Site_id { get; set; }
+        public string Name { get; set; }
+        public DateTime From_date { get; set; }
+        public DateTime To_date { get; set; }
+        public DateTime Create_date { get; set; }
+
+        public override string ToString()
+        {
+            return "";
+        }
+    }
+}
