f7f5bad1bd6f0c2d3f50d501158c77dd3b50429c
Grant Payne <afroman7600@yahoo.com>
2019-02-21 14:44:45 -0500

    Completed GetInteger, GetBool, and GetString methods - GetDate metho in progress

---
+++ 08-Capstone/Capstone/CLIHelper.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Capstone
+{
+    class CLIHelper
+    {
+        public static int GetInteger(string message)
+        {
+            string userInput = String.Empty;
+            int intValue = 0;
+            int numberOfAttempts = 0;
+
+            do
+            {
+                if (numberOfAttempts > 0)
+                {
+                    Console.WriteLine("Invalid input format. Please try again");
+                }
+
+                Console.Write(message + " ");
+                userInput = Console.ReadLine();
+                numberOfAttempts++;
+            }
+            while (!int.TryParse(userInput, out intValue));
+
+            return intValue;
+
+        }
+
+        public static bool GetBool(string message)
+        {
+            string userInput = String.Empty;
+            bool boolValue = false;
+            int numberOfAttempts = 0;
+
+            do
+            {
+                if (numberOfAttempts > 0)
+                {
+                    Console.WriteLine("Invalid input format. Please try again");
+                }
+
+                Console.Write(message + " ");
+                userInput = Console.ReadLine();
+                numberOfAttempts++;
+            }
+            while (!bool.TryParse(userInput, out boolValue));
+
+            return boolValue;
+        }
+
+        public static string GetString(string message)
+        {
+            string userInput = String.Empty;
+            int numberOfAttempts = 0;
+
+            do
+            {
+                if (numberOfAttempts > 0)
+                {
+                    Console.WriteLine("Invalid input format. Please try again");
+                }
+
+                Console.Write(message + " ");
+                userInput = Console.ReadLine();
+                numberOfAttempts++;
+            }
+            while (String.IsNullOrEmpty(userInput));
+
+            return userInput;
+        }
+
+        //TODO: Finish method to parse user date input into SQL-useable date string
+
+        //public static string CreateSqlDateInput(string year, string month, string day)
+        //{
+        //    string userInput = String.Empty;
+        //    int numberOfAttempts = 0;
+
+        //    do
+        //    {
+
+        //        if (numberOfAttempts > 0)
+        //        {
+        //            Console.WriteLine("Invalid input format. Please try again");
+        //        }
+
+                
+
+        //    } while (String.IsNullOrEmpty(userInput));
+        //}
+    }
+}
